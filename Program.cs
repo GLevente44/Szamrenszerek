eb49916b01ba3577d093917c321fe93cff182213
GecseLevente <gecsel@kkszki.hu>
2024-10-04 18:37:34 +0200

    szamrendszerek project is ready to use

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamrendszerek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérlek, adj meg egy egész számot a tízes számrendszerben:");
+            int szam = int.Parse(Console.ReadLine());
+
+            string kettesSzamrendszer = Atvaltas(szam);
+
+            Console.WriteLine($"A {szam} szám kettes számrendszerben: {kettesSzamrendszer}");
+        }
+
+        static string Atvaltas(int szam)
+        {
+            if (szam == 0)
+                Console.WriteLine("0");
+
+            List<int> maradek = new List<int>();
+            while (szam > 0)
+            {
+                maradek.Add(szam % 2); 
+                szam /= 2; 
+            }
+
+            return string.Join("", maradek);
+        }
+    }
+}
+    
+
